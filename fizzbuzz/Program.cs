69ee0dcf2c5fcb3f83605303fa55366e576800aa
l_md_l <maks.degtearyov25@gmail.ru>
2022-05-15 20:55:41 +0300

    fizzbuzz code

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿int num = 1;
+for (; num <=100; num++)
+{
+    if (num % 3==0 && num % 5==0)
+    {
+        Console.WriteLine("fizzbuzz");
+    }
+    else if (num % 3==0)
+    {
+        Console.WriteLine("fizz");
+    }
+    else if (num % 5==0)
+    {
+        Console.WriteLine("buzz");
+    }
+    else
+    {
+        Console.WriteLine(num);
+    }
+}
