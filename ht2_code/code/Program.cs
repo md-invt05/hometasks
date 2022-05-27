d41852754057442235aff1df56bdfd5b51968f92
l_md_l <maks.degtearyov25@gmail.ru>
2022-05-27 19:20:35 +0300

    new code

---
+++ ht2_code/code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+//Немного не по заданию, хотел адаптировать для любого текста
+Console.WriteLine("Введите текст без абзацев");// В консоль почему-то не вставляется с абзацами
+string text = Console.ReadLine();
+Console.WriteLine("Введите слово, которое хотите найти");
+string word = Console.ReadLine();
+
+Regex rx = new Regex(word,RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+MatchCollection matches = rx.Matches(text);
+int nummber = matches.Count;
+Console.WriteLine(nummber);
